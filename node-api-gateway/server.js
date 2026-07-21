fde29a4932e4443012d7416f5f542d9dca95e1f5
Nikhil <niktemporary69@gmail.com>
2026-07-21 17:20:10 +0530

    Wired up the connectToEngine, middlewares, route and listen to the server file.

--- node-api-gateway/server.js
+++ node-api-gateway/server.js
@@ -1,17 +1,25 @@
+import "dotenv/config";
+import express from "express";
+import { connectToEngine } from "./src/engineConnection.js";
+import { blockMiddleware } from "./src/middlewares/blockMiddleware.js";
+import { getBlocklistSize } from "./src/utils/Blocklist.js";
 
-import 'dotenv/config'
-import express from 'express'
+const app = express();
+const PORT = process.env.PORT;
 
-const app = express()
-const PORT = process.env.PORT
-
-
+connectToEngine();
 
 app.use(express.json());
+app.use(blockMiddleware);
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Request allowed.", ip: req.ip });
+});
+
+app.get("/api/status", (req, res) => {
+  res.json({ blockedIpCount: getBlocklistSize() });
+});
 
 app.listen(PORT, () => {
-  console.log('Server is running on http://localhost:3000')
-})
-
-
-
+      console.log("Server is running on http://localhost:3000");
+});
